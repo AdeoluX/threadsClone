f6ffbf3f00c7f15d0d4c25c1661ca7e718c7ce46
TAdejuwon <tadejuwon@chapelhilldenham.com>
2024-05-02 16:23:46 +0100

    first commit

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,15 +1,49 @@
-import { StyleSheet } from 'react-native';
-
+import * as React from 'react';
+import { Platform, RefreshControl, SafeAreaView, ScrollView, StyleSheet } from 'react-native';
 import EditScreenInfo from '@/components/EditScreenInfo';
 import { Text, View } from '@/components/Themed';
+import LottieView from 'lottie-react-native';
+import { createRandomUser } from '@/utils/generate-dummy-data';
+import { ThreadsContext } from '@/context/thread-context';
+import ThreadsItem from '@/components/ThreadsItem';
+
 
 export default function TabOneScreen() {
+  const animationRef = React.useRef<LottieView>(null)
+  const threads = React.useContext(ThreadsContext)
   return (
-    <View style={styles.container}>
-      <Text style={styles.title}>Tab One</Text>
-      <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
-      <EditScreenInfo path="app/(tabs)/index.tsx" />
-    </View>
+    <SafeAreaView 
+      // style={{flex: 1}}
+      >
+      <ScrollView
+      // style={{flex: 1}}
+        contentContainerStyle = {{
+          // backgroundColor: 'gray',
+          paddingHorizontal: 10,
+          paddingTop: Platform.select({android: 30})
+        }}
+        refreshControl={<RefreshControl refreshing = {false}
+          onRefresh={() => {animationRef.current?.play()}}
+          tintColor="transparent"
+        />}
+      >
+        <LottieView
+          ref = {animationRef}
+          source={require("../../lottie-animation/threads.json")}
+          loop={false}
+          autoPlay
+          style={{width: 90, height: 90, alignSelf: 'center'}}
+          // onAnimationFinish={() => {
+          //   alert("Finished")
+          // }}
+        />
+        {
+          threads.map((thread) => (
+            <ThreadsItem key={thread.id} {...thread}/>
+          ))
+        }
+      </ScrollView>
+    </SafeAreaView>
   );
 }
 
